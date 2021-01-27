4daeecbd15e557bd1a41871c06054bf405cd3eed
laurirantasalo <Rantasalo.Lauri@gmail.com>
2021-01-27 15:16:40 +0200

    Simple first iteration

---
+++ PersonalWebsite/src/app/app-routing-module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
+
+
+const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'projects', component: ProjectsComponent},
+    {path: 'resume', component: ResumeComponent},
+    {path: 'contact', component: ContactComponent},
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
---
+++ PersonalWebsite/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './app-routing-module';
+
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'Resume', component: ResumeComponent},
+      {path: 'projects', component: ProjectsComponent},
+      {path: 'contact', component: ContactComponent}
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
